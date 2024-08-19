fd998a206578042943c8b7308fb93da845318466
moop <katycatt6@gmail.com>
2024-08-20 00:10:26 +0200

    added music and main title screen!

---
+++ ui/user_interface.gd
@@ -0,0 +1,12 @@
+extends ColorRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	global.add_text_to_dot_matrix("Well. Seems like it's online. Now to test NanoBot #42 out... Does it work? Power on, #42!")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
